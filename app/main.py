cb915fa46fb1bf49e4f9a90ad260be53e23a1f86
Julien <julien.lejay@gmail.com>
2025-07-31 14:10:11 +0700

    feat: add upload cert

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, UploadFile, File, Request
 from app.vespa_client import query_vespa
+import shutil
 
 app = FastAPI()
 
@@ -11,3 +12,11 @@ async def query_rag(req: Request):
     print(query)
     results = query_vespa(query)
     return {"chunks": results}
+
+
+@app.post("/admin/upload-cert/")
+async def upload_cert(file: UploadFile = File(...)):
+    save_path = f"/app/certs/{file.filename}"
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"filename": file.filename, "status": "uploaded"}
